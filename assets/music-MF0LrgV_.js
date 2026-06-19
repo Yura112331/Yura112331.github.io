f7a80447b17163bb90ef7bd031ca6cbf8d05652a
Yura112331 <yura.valyavsky@gmail.com>
2026-06-19 22:58:38 +0300

    Updates

--- assets/music-MF0LrgV_.js
+++
@@ -1,1 +0,0 @@
-import{n as e}from"./index-BQ18UPb4.js";var t=e(`MusicIcon`,[[`path`,{d:`M9 18V5l12-2v13`,key:`1jmyc2`}],[`circle`,{cx:`6`,cy:`18`,r:`3`,key:`fqmcym`}],[`circle`,{cx:`18`,cy:`16`,r:`3`,key:`1hluhg`}]]);export{t};
