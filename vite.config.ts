12c0404bde33b991c5892837ad3a2a05bbcf63d3
Yura112331 <yura.valyavsky@gmail.com>
2026-06-19 22:01:53 +0300

    n

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import vue from '@vitejs/plugin-vue'
 import { fileURLToPath, URL } from 'node:url'
 
 export default defineConfig({
-  base: '/',
+  base: '/Yura112331.github.io/',
   plugins: [vue()],
   resolve: {
     alias: {
